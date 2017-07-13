8828cbcf4dba3e4c5f94ebbc2aa5833bb5aa7be8
Alfo92 <zuni_alfo92@hotmail.com>
2017-07-13 14:45:12 -0400

    Nuevo

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+ get 'formularios/por_post' => 'formularios#por_post'
+post 'formularios/por_post' => 'formularios#por_pos'
+# get 'formularios/por_post'
+
+ #get 'redes_sociales/facebook'
+
+ # get 'redes_sociales/twitter'
+
+ #get 'redes_sociales/google_mas'
+
+ # post 'formularios/por_post'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
